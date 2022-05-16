cda93fd92694f3f7c4477a3ac1ceeb01d7b92b41
Shivang Shandilya <shivangshandilya83@gmail.com>
2022-05-16 16:21:08 +0530

    commit

---
+++ build/precache-manifest.83f47e771d5b3d01364f9ce999dabc6a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e9b13f59f10cfdc28a15443e797c73",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/KeeperApp/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "0d312ae9356af86c475d",
+    "url": "/KeeperApp/static/js/main.a3c928bb.chunk.js"
+  },
+  {
+    "revision": "f87f7f290d4c0085841d",
+    "url": "/KeeperApp/static/js/runtime-main.9f8bc1f0.js"
+  }
+]);
